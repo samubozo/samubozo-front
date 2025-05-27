5d5d1c23e9a7c96e97604faa499dd796cd41a675
LeeKM321 <stephen4951@gmail.com>
2025-05-27 19:23:45 +0900

    SSE 연결을 위한 EventSourcePolyfill 생성 및 이벤트 처리

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,40 @@ import {
   Toolbar,
   Typography,
 } from '@mui/material';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import AuthContext from '../context/UserContext';
+import { EventSourcePolyfill } from 'event-source-polyfill';
+import { API_BASE_URL, ORDER, SSE } from '../configs/host-config';
 
 const Header = () => {
   // 로그인 상태에 따라 메뉴를 다르게 제공하고 싶다 -> Context에서 뽑아오면 되겠구나!
   const { isLoggedIn, onLogout, userRole } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log('role: ', userRole);
+    const token = localStorage.getItem('ACCESS_TOKEN');
+
+    if (userRole === 'ADMIN') {
+      // 알림을 받기 위해 서버와 연결을 하기 위한 요청을 하겠다.
+      // 기존에 사용하던 fetch, axios는 지속적 연결을 지원하지 않는다.
+      const sse = new EventSourcePolyfill(`${API_BASE_URL}${SSE}/subscribe`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      sse.addEventListener('connect', (event) => {
+        console.log(event);
+      });
+
+      // 30초마다 발생하는 알림. (연결을 유지하기 위해)
+      sse.addEventListener('heartbeat', () => {
+        console.log('Received heartbeat');
+      });
+    }
+  }, [userRole]);
 
   const handleLogout = () => {
     onLogout();
