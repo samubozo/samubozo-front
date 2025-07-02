a816ce0e3cc182109466f26e0c4e7fbf91b41cf4
hoyeong-i <ghdud939@gmail.com>
2025-07-02 22:50:53 +0900

    챗봇 UI 추가

--- src/components/LayoutHeader.jsx
+++ src/components/LayoutHeader.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import Chatbot from './Chatbot';
 
 const LayoutHeader = () => {
   return (
     <div>
       <Header />
-      <Outlet/>
+      <Outlet />
+      <Chatbot />
     </div>
   );
 };
