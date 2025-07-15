ceed91ba08416623925c2a90b9cc1c40ab028ba5
hoyeong-i <ghdud939@gmail.com>
2025-07-15 09:29:54 +0900

    리액트 19버전으로 다시 수정. 일정관리 수정(캘린터 카테고리 없으면 일정 추가버튼 비활성화)

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.render(
+// React 19의 createRoot API 사용
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
 );
