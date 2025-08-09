3d2821416e283f4bbda97d60d28a791555ce7d37
hoyeong-i <ghdud939@gmail.com>
2025-08-09 17:11:09 +0900

    인증받은 백엔드 주소로 변경

--- src/configs/host-config.js
+++ src/configs/host-config.js
@@ -22,7 +22,7 @@ if (
   clientHostName === 'www.samubozo.co.kr'
 ) {
   // 배포해서 현재 서비스 중
-  backendHostName = 'https://api.samubozo.co.kr';
+  backendHostName = 'https://apiv1.samubozo.co.kr';
 }
 
 export const API_BASE_URL = backendHostName;
